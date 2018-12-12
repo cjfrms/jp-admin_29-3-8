ee60e6229a22c209e7623b885f28185bb7bc831a
geuni <cjfrms@live.com>
2018-12-12 16:19:53 +0800

    X

--- src/app/features/schedule/schedule.module.ts
+++ src/app/features/schedule/schedule.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { ScheduleManagementComponent } from './schedule-management/schedule-management.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    NgZorroAntdModule,
     RouterModule.forChild([{
       path: '',
-      component: ScheduleManagementComponent
+      component: ScheduleManagementComponent,
+      data: {
+        breadcrumb: 'Display Name'
+      }
     }]),
   ],
   declarations: [ScheduleManagementComponent]
