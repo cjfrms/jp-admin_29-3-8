4b00e8bdd86411a6d8e3c8c8b69670dcde7df98b
geuni <cjfrms@live.com>
2018-12-28 14:28:32 +0800

    x

--- src/app/features/schedule/schedule-management/schedule-management.component.ts
+++ src/app/features/schedule/schedule-management/schedule-management.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ScheduleService } from '../schedule.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ValidationErrors,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-schedule-management',
@@ -8,9 +16,40 @@ import { Component, OnInit } from '@angular/core';
 export class ScheduleManagementComponent implements OnInit {
 
   dataSet ;
-  constructor() { }
+  isVisible = false;
+  validateForm: FormGroup;
+
+  submitForm = ($event, value) => {
+    $event.preventDefault();
+    for (const key in this.validateForm.controls) {
+      this.validateForm.controls[ key ].markAsDirty();
+      this.validateForm.controls[ key ].updateValueAndValidity();
+    }
+    console.log(value);
+  }
+
+  constructor(private scheduleService: ScheduleService, private fb: FormBuilder) {
+    this.validateForm = this.fb.group({
+      userName: [ '', [ Validators.required ]]
+    });
+
+  }
 
   ngOnInit() {
+    this.scheduleService.getScheduleList().subscribe(data => {
+
+      // cache our list
+      this.dataSet = data;
+    });
   }
+
+  showModal(): void {
+    this.isVisible = true;
+  }
+
+  cancelModalWin(): void {
+    this.isVisible = false;
+  }
+
 
 }
--- src/app/features/schedule/schedule.module.ts
+++ src/app/features/schedule/schedule.module.ts
@@ -3,11 +3,14 @@ import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
 import { ScheduleManagementComponent } from './schedule-management/schedule-management.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
     NgZorroAntdModule,
+    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forChild([{
       path: '',
       component: ScheduleManagementComponent,
